8a516f9360fed93c6c59b98183a1d0bbeb0efcd6
RxNumeric <71637680+RxNumeric@users.noreply.github.com>
2024-05-11 16:32:50 +0700

    Add files via upload

---
+++ myaiwebapp1.py
@@ -0,0 +1,30 @@
+import streamlit as st
+
+import google.generativeai as genai
+#genai.configure(api_key="AIzaSyCJA1hPNHpbNmU89AAiyAqTqPhZ7Pa_1Dk")
+genai.configure(api_key="AIzaSyC7LZJxaD-oKcwIxUDRGJHxaw2H1hkKf1s")
+model = genai.GenerativeModel("gemini-pro")
+
+st.title("แปลภาษา")
+ch = st.selectbox("เลือกภาษาปลายทาง",
+                 ("ไทย","อังกฤษ","เกาหลี","ญี่ปุ่น"))
+
+text_in = st.text_input("ป้อนข้อความที่ต้องการแปล: ")
+
+prompt = "แปลข้อความต่อไปนี้เป็นภาษา"+ ch + " " + text_in
+st.text(prompt)
+
+if st.button("แปล"):
+    try:
+        response = model.generate_content(prompt)
+        st.text(response.text)
+    except:
+        st.text("no response")
+
+
+
+
+    
+
+
+
